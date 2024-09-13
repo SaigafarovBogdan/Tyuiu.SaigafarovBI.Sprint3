8e95382933025a73a290877a81e2b2d7555a9a74
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-13 23:14:01 +0500

    Task0

--- Tyuiu.SaigafarovBI.Sprint3.Task0.V26.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint3.Task0.V26.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.SaigafarovBI.Sprint3.Task0.V26.Lib
         public double GetMultiplySeries(int startValue, int stopValue)
         {
             double res = 1.0;
-            for (int i = startValue; i < stopValue; ++i)
+            for (int i = startValue; i <= stopValue; ++i)
             {
                 res *= (Math.Pow(2.0, i) / (i + 1.0)) * Math.Cos(1.8);
             }
