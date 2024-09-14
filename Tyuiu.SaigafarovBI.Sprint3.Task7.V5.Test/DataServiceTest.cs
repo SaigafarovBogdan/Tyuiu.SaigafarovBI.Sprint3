a37e1b356699124c89175b0394157fabfaf0a35a
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-14 22:03:08 +0500

    Task7

--- Tyuiu.SaigafarovBI.Sprint3.Task7.V5.Test/DataServiceTest.cs
+++ Tyuiu.SaigafarovBI.Sprint3.Task7.V5.Test/DataServiceTest.cs
@@ -13,17 +13,17 @@ namespace Tyuiu.SaigafarovBI.Sprint3.Task7.V5.Test
             int stopValue = 5;
             double[] valueWaitArray = new double[stopValue-startValue+1];
             valueWaitArray[0] = 70.14;
-            valueWaitArray[1] = 55.21;
+            valueWaitArray[1] = 55.20;
             valueWaitArray[2] = 41.05;
-            valueWaitArray[3] = 27.96;
-            valueWaitArray[4] = 15.48;
+            valueWaitArray[3] = 27.99;
+            valueWaitArray[4] = 15.66;
             valueWaitArray[5] = 1.0;
-            valueWaitArray[6] = -13.06;
-            valueWaitArray[7] = -28.16;
+            valueWaitArray[6] = -13.09;
+            valueWaitArray[7] = -28.17;
             valueWaitArray[8] = -42.96;
             valueWaitArray[9] = -56.77;
             valueWaitArray[10] = -69.83;
-            Assert.AreEqual(dataService.GetMassFunction(startValue, stopValue), valueWaitArray);
+            CollectionAssert.AreEqual(dataService.GetMassFunction(startValue, stopValue), valueWaitArray);
 
         }
     }
