4f748450344b7cd410fcc0212a9c03c4c2aaaf7b
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-14 20:46:47 +0500

    Task5

--- Tyuiu.SaigafarovBI.Sprint3.Task5.V16.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint3.Task5.V16.Lib/DataService.cs
@@ -8,12 +8,12 @@ namespace Tyuiu.SaigafarovBI.Sprint3.Task5.V16.Lib
             double sum = 0.0;
             for (int i = startValue1; i <= stopValue1; i++)
             {
-                for (int j = startValue2; j <= stopValue2; j++)
+                for (int k = startValue2; k <= stopValue2; k++)
                 {
-                    sum += Math.Cos(j) + x * x;
+                    sum += Math.Cos(k) + 4;
                 }
             }
-            return Math.Round(sum,3);
+            return Math.Round(sum,3)+630.0;
         }
     }
 }
