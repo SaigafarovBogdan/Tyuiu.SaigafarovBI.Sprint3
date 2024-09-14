d3f0eee250c8e51addb55845f961e663aa54f289
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-14 21:58:54 +0500

    Task7

---
+++ Tyuiu.SaigafarovBI.Sprint3.Task7.V5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tyuiu.SaigafarovBI.Sprint3.Task7.V5.Lib;
+namespace Tyuiu.SaigafarovBI.Sprint3.Task7.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService dataService = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            double[] valueArray = new double[stopValue - startValue + 1];
+            valueArray = dataService.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for(int i = 0; i<= valueArray.Length-1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
